3c1be57a6cb1993ff0c2cf6e2a05d634e4b82ed9
Amit Jain <ajonnet@gmail.com>
2021-04-22 21:23:27 +0530

    Initial Commit

---
+++ SnapGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SnapGenerator
+//
+//  Created by Amit Jain on 22/04/21.
+//  Copyright © 2021 ajonnet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
